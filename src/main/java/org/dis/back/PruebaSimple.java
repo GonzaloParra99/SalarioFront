75a5632b0433e77237259a62116b2ac6000e4fff
Gonzalo <gonzalo.parra124@gmail.com>
2024-11-07 20:21:44 +0100

    Primer commit

---
+++ src/main/java/org/dis/back/PruebaSimple.java
@@ -0,0 +1,13 @@
+package org.dis.back;
+
+public class PruebaSimple {
+    //Devuelve verdadero si el numero a es mayor que el numero b
+    //falso en caso contrario
+    public boolean prueba1 (int a, int b){
+        if (a > b){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/main/java/org/dis/front/MyUI.java
@@ -0,0 +1,48 @@
+package org.dis.front;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+
+@Theme("mytheme")
+public class MyUI extends UI {
+
+    private TextField creaLabel(String texto){
+        TextField etiqueta = new TextField();
+        ;etiqueta.setCaption(texto);
+        return etiqueta;
+    }
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        TextField tipo = creaLabel("Tipo de empleado");
+        TextField horasExtra = creaLabel("Horas trabajadas");
+        TextField ventasMes = creaLabel("Ventas realizadas");
+
+
+        Button button = new Button("Calcular");
+        button.addClickListener(e -> {
+
+        });
+        
+        layout.addComponents(tipo, horasExtra, ventasMes, button);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
